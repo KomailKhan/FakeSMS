dc9457dc6e9c1a54e35d3f2998d7e0a77a902112
Komail Khan <64368068+KomailKhan@users.noreply.github.com>
2020-06-01 00:00:37 +0500

    Update TBomb.sh

--- TBomb.sh
+++ TBomb.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 clear
-echo -e "\e[4;31m SpeedX Productions !!! \e[0m"
+echo -e "\e[4;31m KomailKhan Productions !!! \e[0m"
 echo -e "\e[1;34m Presents \e[0m"
-echo -e "\e[1;32m TBomb \e[0m"
+echo -e "\e[1;32m FakeSMS \e[0m"
 echo "Press Enter To Continue"
 read a1
-if [[ -s update.speedx ]];then
+if [[ -s update.komailkhan ]];then
 echo "All Requirements Found...."
 else
 echo 'Installing Requirements....'
@@ -14,7 +14,7 @@ echo .
 apt install figlet toilet python curl -y
 apt install python3-pip
 pip install -r requirements.txt
-echo This Script Was Made By SpeedX >update.speedx
+echo This Script Was Made By KomailKhan >update.komailkhan
 echo Requirements Installed....
 echo Press Enter To Continue...
 read upd
@@ -24,13 +24,13 @@ do
 rm *.xxx >/dev/null 2>&1
 clear
 echo -e "\e[1;31m"
-figlet TBomb
+figlet F-SMS
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border SpeedX
-echo -e "\e[4;34m This Bomber Was Created By SpeedX \e[0m"
+toilet -f mono12 -F border Komail
+echo -e "\e[4;34m This Bomber Was Created By KomailKhan \e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
+echo -e "\e[1;32m           Mail: mysweet.sparrow393@gmail.com \e[0m"
+echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/UCDJbhYSPToi1-CdzGLEzAIQ \e[0m"
 echo " "
 echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"
 echo " "
@@ -50,7 +50,7 @@ exit 0
 elif [ $ch -eq 2 ];then
 clear
 echo -e "\e[1;32m"
-echo 'Call Bomb By SpeedX'> call.xxx
+echo 'Call Bomb By Komail'> call.xxx
 python3 bomber.py call
 rm *.xxx >/dev/null 2>&1
 exit 0
@@ -58,17 +58,17 @@ elif [ $ch -eq 3 ];then
 clear
 apt install git -y
 echo -e "\e[1;34m Downloading Latest Files..."
-git clone https://github.com/TheSpeedX/TBomb
+git clone https://github.com/KomailKhan/Fake-SMS
 if [[ -s TBomb/TBomb.sh ]];then
-cd TBomb
+cd FakeSMS
 cp -r -f * .. > temp
 cd ..
-rm -rf  TBomb >> temp
-rm update.speedx >> temp
+rm -rf  FakeSMS >> temp
+rm update.Komailkhan >> temp
 rm temp
 chmod +x TBomb.sh
 fi
-echo -e "\e[1;32m TBomb Will Restart Now..."
+echo -e "\e[1;32m FakeSMS Will Restart Now..."
 echo -e "\e[1;32m All The Required Packages Will Be Installed..."
 echo -e "\e[1;34m Press Enter To Proceed To Restart..."
 read a6
@@ -77,9 +77,9 @@ exit
 elif [ $ch -eq 4 ];then
 clear
 echo -e "\e[1;33m"
-figlet TBomb
+figlet F-SMS
 echo -e "\e[1;34mCreated By \e[1;34m"
-toilet -f mono12 -F border SpeedX
+toilet -f mono12 -F border Komail
 echo  " "
 echo -e "\e[1;32m                   Features\e[1;34m"
 echo "  [+] Unlimited And Super-Fast Bombing"
@@ -90,38 +90,38 @@ echo "  [+] Automated Future Updates"
 echo "  [+] Easy To Use And Embed in Code"
 echo -e "\e[1;32m                   Contributors\e[1;33m"
 echo -e "\e[1;33m      [*]  SpeedX   \e[1;31m"
-echo "         [-] Mail At: ggspeedx29@gmail.com"
-echo -e "\e[1;33m      [*]  The Black Hacker Roxstar   \e[1;31m"
-echo "         [-] Ping At: http://wa.me/917600140353"
+echo "         [-] Mail At: mysweet.sparrow393@gmail.com"
+echo -e "\e[1;33m      [*]  The White Tiger Komail Roxstar   \e[1;31m"
+echo "         [-] Ping At: http://wa.me/+923124788959"
 echo -e "\e[1;33m      [*]  Rieltar   \e[1;31m"
 echo "         [-] Ping At: https://t.me/Rieltar"
-echo -e "\e[1;33m      [*]  0n1cOn3 (Stefan)   \e[1;31m"
-echo "         [-] Mail At: 0n1cOn3@gmx.ch"
+echo -e "\e[1;33m      [*]  Youtube (Freaked Dude)   \e[1;31m"
+echo "         [-] Mail At: mysweet.sparrow393@gmail.com"
 echo ""
 echo ""
 echo -e "\e[1;31m This Script is Only For Educational Purposes or To Prank.\e[0m"
 echo -e "\e[1;31m Do not Use This To Harm Others. \e[0m"
-echo -e "\e[1;31m I Am Not Responsible For The Misuse Of The Script. \e[0m"
+echo -e "\e[1;31m Komail Khan is Not Responsible For The Misuse Of The Script. \e[0m"
 echo -e "\e[1;32m Make Sure To Update it If It Does not Work.\e[0m"
 echo  " "
 echo -e "\e[4;31m That's All !!!\e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[1;32m       Whatsapp: https://bit.do/speedxgit \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
+echo -e "\e[1;32m           Mail: mysweet.sparrow393@gmail.com \e[0m"
+echo -e "\e[1;32m       Whatsapp: +923124788959"
+echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/UCDJbhYSPToi1-CdzGLEzAIQ \e[0m"
 echo "Press Enter To Go Home"
 read a3
 clear
 elif [ $ch -eq 5 ];then
 clear
 echo -e "\e[1;31m"
-figlet TBomb
+figlet F-SMS
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border SpeedX
+toilet -f mono12 -F border Komail
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[1;32m       Whatsapp: https://bit.do/speedxgit \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
+echo -e "\e[1;32m           Mail: mysweet.sparrow393@gmail.com \e[0m"
+echo -e "\e[1;32m       Whatsapp: +923124788959"
+echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/UCDJbhYSPToi1-CdzGLEzAIQ \e[0m"
 echo " "
 exit 0
 else
